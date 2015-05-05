103ba0fe2a0701000c66704c0592e06af8a21f3f
George Mauer <gmauer@gmail.com>
2015-05-05 00:31:47 -0500

    basic project template with no implementation

--- rosterHelpers.js
+++ rosterHelpers.js
@@ -1,20 +1,42 @@
 (function() {
-	var groupPlayersByPosition = function(players) {
-	  return _.map(_.groupBy(players, 'position'), function(players, position) { 
-	    return {
-	      position: position,
-	      players: players
-	    };
-	  });
+
+	var groupByProperty = function(group, propertyName) {
+		if(!_.isArray(group) || !_.isString(propertyName)) {
+			throw Error("You must pass in an array of object and a string property eg groupPlayersByProperty(players, 'position')")
+		}
+		group.map(function(p, index){
+			if(!_.has(p, propertyName)) {
+				console.warn("Note the object at array index", index, "does not have the", propertyName, "property")
+			}
+		});
+	  	return _.map(_.groupBy(group, propertyName), function(group, propertyValue) { 
+	    	var x = { };
+	    	x[propertyName] = propertyValue;
+	      	x.group = group;
+	      	return x;
+	  	});
 	};
+
 	var sum = function(values) {
 	  return values.reduce(function(a,b) { 
 	    return a + b;
 	  }, 0);
 	};
 
+	var showInList = function(selector, listOfStrings) {
+		var element = document.querySelector(selector);
+		if(!element) {
+			throw Error("No element found that matches the selector '" + selector + "'");
+		}
+		element.innerHTML = listOfStrings.map(function(s) { 
+			return "<li>" + s + "</li>"; 
+		}).join('\n');
+	};
+
+
 	window.rosterHelpers = {
-		groupPlayersByPosition: groupPlayersByPosition,
-		sum: sum
+		groupByProperty: groupByProperty,
+		sum: sum,
+		showInList: showInList,
 	};
 }).call();
